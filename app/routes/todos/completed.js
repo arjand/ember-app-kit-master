3f083f1caeaf639b05c05bbcbc99bb021909ff3e
arjand <adehar@gmail.com>
2013-10-21 18:04:53 -0700

    Forgot to add files for todos child routes

---
+++ app/routes/todos/completed.js
@@ -0,0 +1,13 @@
+var TodosCompletedRoute = Ember.Route.extend({
+	model: function () {
+		return this.store.filter('todo', function(todo) {
+			return todo.get('isCompleted');
+		});
+	},
+	renderTemplate: function(controller) {
+		// we want to reuse the existing todos/index template even though route is changing
+		this.render('todos/index', {controller: controller});
+	}	
+});
+
+export default TodosCompletedRoute;
